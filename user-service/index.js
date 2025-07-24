425fc4a176254e2f1353e5758d1d92015c4c399e
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-07-24 10:07:10 +0600

    code update for error

--- user-service/index.js
+++ user-service/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import cors from 'cors';
 
 // import rateLimit from 'express-rate-limit';
-import helmet from "helmet";
+
 import cookieParser from "cookie-parser";
-import hpp from "hpp";
+
 
 import dotenv from "dotenv"
 import userRouter from './routes/userRouter.js'
@@ -24,33 +24,12 @@ app.use(cors({
 }));
 
 
-
-
-
-
-
-
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(hpp());
-app.use(helmet({
-  crossOriginResourcePolicy: { policy: "cross-origin" },
-}));
+
 app.use(cookieParser());
-
-// Rate Limiter
-// const limiter = rateLimit({
-//   windowMs: REQUEST_LIMIT_TIME,
-//   max: REQUEST_LIMIT_NUMBER,
-// });
-// app.use(limiter);
-
-// Web Caching
-// app.set('etag', WEB_CACHE);
 
 
 app.use("/api/auth", userRouter);
