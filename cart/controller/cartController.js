953ea85780e02c4e9278da788ad71fadb814b07f
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-09-08 12:26:11 +0600

    add get cart item in cart service

--- cart/controller/cartController.js
+++ cart/controller/cartController.js
@@ -29,3 +29,59 @@ export const addToCart = async (req, res) => {
     res.status(400).json({ success: false, message: "Failed to add product", error: error.message });
   }
 };
+
+
+
+
+
+export const getCartItems = async (req, res) => {
+  try {
+    const user_id = req.user._id;
+
+    const matchStage = { $match: { userID: new ObjectID(user_id) } };
+    const JoinStageProduct = {
+      $lookup: { from: "products", localField: "productID", foreignField: "_id", as: "product" },
+    };
+    const unwindProductStage = { $unwind: "$product" };
+
+    const JoinStageBrand = {
+      $lookup: { from: "brands", localField: "product.brandID", foreignField: "_id", as: "brand" },
+    };
+    const unwindBrandStage = { $unwind: "$brand" };
+
+    const JoinStageCategory = {
+      $lookup: { from: "categories", localField: "product.categoryID", foreignField: "_id", as: "category" },
+    };
+    const unwindCategoryStage = { $unwind: "$category" };
+
+    const projectionStage = {
+      $project: {
+        userID: 0,
+        createdAt: 0,
+        updatedAt: 0,
+        "product._id": 0,
+        "product.categoryID": 0,
+        "product.brandID": 0,
+        "brand._id": 0,
+        "category._id": 0,
+      },
+    };
+
+    const data = await CartModel.aggregate([
+      matchStage,
+      JoinStageProduct,
+      unwindProductStage,
+      JoinStageBrand,
+      unwindBrandStage,
+      JoinStageCategory,
+      unwindCategoryStage,
+      projectionStage,
+    ]);
+
+    res.status(200).json({ success: true, data });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ success: false, message: "Failed to fetch cart", error: error.message });
+  }
+};
+
