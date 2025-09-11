97b28f031bb2dffddf6b571ab982c0883c925a0b
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-09-11 09:04:33 +0600

    add cart helm in

--- cart/index.js
+++ cart/index.js
@@ -3,9 +3,12 @@ import cors from "cors"
 import cartRouter from './routes/cartRoute.js'
 import { dbConnected } from './utils/db.js'
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv"
 
-dbConnected()
 const app = express()
+dotenv.config()
+const PORT = process.env.PORT
+dbConnected()
 
 app.use(cors({
   origin: 'http://localhost:5173', 
@@ -17,6 +20,6 @@ app.use(cookieParser());
 
 app.use('/api/cart', cartRouter)
 
-app.listen(3002, ()=>{
-    console.log("server is running port 3002");
+app.listen(PORT, ()=>{
+    console.log(`server is running port ${PORT}`);
 })
