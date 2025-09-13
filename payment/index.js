93fdfc3cd09cb7a2ec259a27317b7e839af1f0a2
DeveloperMokchhedul <mokchheduls46@gmail.com>
2025-09-13 14:23:37 +0600

    update payment system code

--- payment/index.js
+++ payment/index.js
@@ -1,23 +1,26 @@
 import express from 'express'
 import cors from "cors"
 import invoiceRouter from "./routes/invoiceRouter.js"
-
 import { dbConnected } from './utils/db.js'
 import cookieParser from 'cookie-parser';
+import dotenv from "dotenv"
 
-dbConnected()
+
 const app = express()
+dotenv.config()
+const PORT = process.env.PORT
+dbConnected();
 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(cors({
   origin: 'http://localhost:5173', 
   credentials: true
 }));
 
-app.use(express.json()); 
-app.use(cookieParser());
-
 app.use('/api/invoice', invoiceRouter)
 
-app.listen(3005, ()=>{
-    console.log("server is running port 3005");
+app.listen(PORT, ()=>{
+    console.log(`server is running port ${PORT}`);
 })
